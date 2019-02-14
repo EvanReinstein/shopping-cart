0105700788ae66a7678cc7e1b81024aca9563952
EvanReinstein <edreins@gmail.com>
2019-02-14 13:31:48 -0800

    Adds shopping cart card and minor style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello Dave...</h1>
+      <div className="container">
+        <Container className="purchase-card">
+          <h1>Hello Dave</h1>
+        </Container>
       </div>
     );
   }
