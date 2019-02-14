638be949d8cad8f4a31887888d05c015f873ade3
EvanReinstein <edreins@gmail.com>
2019-02-14 13:49:11 -0800

    Adds subtotal and in-store pickup savings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
+
+// Components
+import SubTotal from './components/Subtotal/Subtotal';
+import PickupSavings from './components/PickupSavings/PickupSavings';
+
+// CSS Imports
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 100,
+      pickupSavings: -3.85
+    }
+  }
   render() {
     return (
       <div className="container">
         <Container className="purchase-card">
-          <h1>Hello Dave</h1>
+          <SubTotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.pickupSavings}/>
         </Container>
       </div>
     );
